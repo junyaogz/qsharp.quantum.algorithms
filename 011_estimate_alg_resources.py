72645efd9b013d1fc333e114215b07ee40b83292
Jun Yao <jun.yao@live.com>
2026-01-30 14:37:12 +0800

    Update 011_estimate_alg_resources.py

--- 011_estimate_alg_resources.py
+++ 011_estimate_alg_resources.py
@@ -1,6 +1,8 @@
 import qsharp
 from qsharp_widgets import EstimateDetails
+from qsharp_widgets import SpaceChart
 
 result = qsharp.estimate("RunProgram()")
-
 EstimateDetails(result)
+
+SpaceChart(result)
